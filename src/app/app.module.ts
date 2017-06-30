b440fe66ed2553d960a20c2fa2de8ff48636d25f
shiva.shirbhate <shiva.shirbhate@infobeans.com>
2017-06-30 18:14:40 +0530

    header complete with design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule
