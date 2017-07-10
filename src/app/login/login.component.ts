1b68a495783e6169f713039b3687d8375bf5987f
shiva.shirbhate <shiva.shirbhate@infobeans.com>
2017-07-10 18:42:55 +0530

    added upload document file with header navigation file upload function is done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
 	username: string;
 	password: string;
-	constructor(router: RouterModule) {
+	constructor() {
 		this.username = this.login.username;
 		this.password = this.login.password;
 	}
--- src/app/upload-doc/upload-doc.component.ts
+++ src/app/upload-doc/upload-doc.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+// import { FileDropDirective } from 'angular2-file-drop';
 
 @Component({
-  selector: 'app-upload-doc',
-  templateUrl: './upload-doc.component.html',
-  styleUrls: ['./upload-doc.component.css']
+	selector: 'app-upload-doc',
+	templateUrl: './upload-doc.component.html',
+	styleUrls: ['./upload-doc.component.css']
 })
 export class UploadDocComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+	allowDrop(event) {
+		event.preventDefault();
+		return true;
+	}
+	drop(e){
+		console.log(e);
+		e.preventDefault();
+	}
+}
+export class uploadDocData {
+	
 
 }
+// Certified ServiceNow Administrator, Certified Implementation Specialists, ITIL Certified Team Members 
